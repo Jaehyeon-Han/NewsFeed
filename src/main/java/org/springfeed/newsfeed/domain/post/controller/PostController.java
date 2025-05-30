5b51f487dcaf4e4677a850adfd46f4cc5c8a22bc
Gaeso <ghwns2239@gmail.com>
2025-05-30 15:16:10 +0900

    feat: 팔로잉 게시물 조회 및 기간별 조회 기능 추가

--- src/main/java/org/springfeed/newsfeed/domain/post/controller/PostController.java
+++ src/main/java/org/springfeed/newsfeed/domain/post/controller/PostController.java
@@ -7,13 +7,13 @@ import org.springfeed.newsfeed.domain.post.dto.request.UpdatePostRequest;
 import org.springfeed.newsfeed.domain.post.dto.response.PostResponse;
 import org.springfeed.newsfeed.domain.post.service.PostService;
 import org.springfeed.newsfeed.global.config.SessionType;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
@@ -42,7 +42,9 @@ public class PostController {
     public ResponseEntity<Page<PostResponse>> getPostResponsePage(
         @RequestParam(defaultValue = "1") int page,
         @RequestParam(defaultValue = "createdAt") String sortBy,
-        @RequestParam(defaultValue = "desc") String sortDir
+        @RequestParam(defaultValue = "desc") String sortDir,
+        @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+        @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
     ) {
         int size = 10;
 
@@ -53,10 +55,49 @@ public class PostController {
                 sortBy)
         );
 
-        // Fixme: 현재 응답 페이지 0부터 시작함
-        Page<PostResponse> postPage = postService.getPostPage(pageable);
+        Page<PostResponse> postPage = postService.getPostPage(pageable, startDate, endDate);
 
-        return ResponseEntity.status(HttpStatus.OK).body(postPage);
+        // Request에서 받은 페이지값을 그대로 반환해주기 위해 커스텀 페이지 생성.
+        Pageable customPageable = PageRequest.of(page, size, pageable.getSort());
+
+        Page<PostResponse> customPage = new PageImpl<>(
+                postPage.getContent(),
+                customPageable,
+                postPage.getTotalElements()
+        );
+
+        return ResponseEntity.status(HttpStatus.OK).body(customPage);
+    }
+
+    // 팔로우 한 사람의 게시글 조회
+    @GetMapping("/following")
+    public ResponseEntity<Page<PostResponse>> getPostFollowingPage(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @SessionAttribute(name = SessionType.USER) Long currentId,
+            @RequestParam(defaultValue = "createdAt") String sortBy,
+            @RequestParam(defaultValue = "desc") String sortDir
+    ) {
+
+        Pageable pageable = PageRequest.of(
+                page - 1,
+                size,
+                Sort.by(Sort.Direction.fromString(sortDir),
+                        sortBy)
+        );
+
+        Page<PostResponse> postPage = postService.getPostFollowingPage(pageable, currentId);
+
+        // Request에서 받은 페이지값을 그대로 반환해주기 위해 커스텀 페이지 생성.
+        Pageable customPageable = PageRequest.of(page, size, pageable.getSort());
+
+        Page<PostResponse> customPage = new PageImpl<>(
+                postPage.getContent(),
+                customPageable,
+                postPage.getTotalElements()
+        );
+
+        return ResponseEntity.status(HttpStatus.OK).body(customPage);
     }
 
     // 게시글 단건 조회
--- src/main/java/org/springfeed/newsfeed/domain/post/repository/PostRepository.java
+++ src/main/java/org/springfeed/newsfeed/domain/post/repository/PostRepository.java
@@ -2,14 +2,30 @@ package org.springfeed.newsfeed.domain.post.repository;
 
 import org.springfeed.newsfeed.domain.entity.Post;
 import org.springfeed.newsfeed.global.error.exception.PostNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     default Post findPostByIdOrElseThrow(Long postId) {
         return findById(postId).orElseThrow(PostNotFoundException::new);
     }
+
+    Page<Post> findAllByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
+
+    @Query("""
+        SELECT p FROM Post p
+        WHERE p.author.id IN (
+            SELECT f.following.id FROM Follow f
+            WHERE f.follower.id = :currentId
+        )
+    """)
+    Page<Post> findPostsByFollowings(@Param("currentId") Long currentId, Pageable pageable);
+
 }
