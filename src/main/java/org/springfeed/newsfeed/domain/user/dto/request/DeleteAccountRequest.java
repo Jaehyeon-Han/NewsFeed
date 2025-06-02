7dd1ca4ea499d9e48cf28cdf9c48b7a6addfaf8c
Jaehyeon Han <jaehyeonhan99@gmail.com>
2025-06-02 17:54:54 +0900

    refactor: 이메일과 비밀번호 입력값 검증에 커스텀 어노테이션 사용

--- src/main/java/org/springfeed/newsfeed/domain/user/dto/request/DeleteAccountRequest.java
+++ src/main/java/org/springfeed/newsfeed/domain/user/dto/request/DeleteAccountRequest.java
@@ -3,12 +3,13 @@ package org.springfeed.newsfeed.domain.user.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
+import org.springfeed.newsfeed.global.annotation.ValidPasswordPattern;
 
 // 회원 탈퇴 시 들어올 정보
 @Getter
 public class DeleteAccountRequest {
 
     @NotBlank(message = "비밀번호를 입력하세요.")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{8,}$", message = "비밀번호 형식이 올바르지 않습니다. 8자 이상, 대소문자 포함, 숫자 및 특수문자(@$!%*?&#) 포함")
+    @ValidPasswordPattern
     private String password;
 }
--- src/main/java/org/springfeed/newsfeed/domain/user/dto/request/SignUpRequest.java
+++ src/main/java/org/springfeed/newsfeed/domain/user/dto/request/SignUpRequest.java
@@ -4,21 +4,23 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import org.springfeed.newsfeed.global.annotation.ValidEmailPattern;
+import org.springfeed.newsfeed.global.annotation.ValidPasswordPattern;
 
 // 회원가입 시 들어올 정보
 @Getter
 public class SignUpRequest {
 
     @NotBlank(message = "이메일을 입력하세요.")
-    @Pattern(regexp = "^[\\w!#$%&'*+/=?`{|}~^.-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
+    @ValidEmailPattern
     private String email;
 
     @NotBlank(message = "비밀번호를 입력하세요.")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{8,}$", message = "비밀번호 형식이 올바르지 않습니다. 8자 이상, 대소문자 포함, 숫자 및 특수문자(@$!%*?&#) 포함")
+    @ValidPasswordPattern
     private String password;
 
     @NotBlank(message = "비밀번호 확인을 입력하세요.")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{8,}$", message = "비밀번호 형식이 올바르지 않습니다. 8자 이상, 대소문자 포함, 숫자 및 특수문자(@$!%*?&#) 포함")
+    @ValidPasswordPattern
     private String passwordCheck;
 
     @NotBlank(message = "닉네임을 입력하세요.")
